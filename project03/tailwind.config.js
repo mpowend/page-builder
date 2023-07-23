6e88c3f7ab3fbc1f3613066407c970c1f753968d
NimaSaei379 <saeinima@yahoo.com>
2023-07-23 10:33:09 +0330

    build the create form

--- project03/tailwind.config.js
+++ project03/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/*.html', './src/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        vazirmatn: ['vazirmatn'],
+      },
+    },
   },
   plugins: [],
 }
