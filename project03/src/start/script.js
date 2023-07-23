d2088497d71bdb2f9a62e51ab027500f813bed5d
Mahdi Pourtalebi <mahdi.pourtalebi@gmail.com>
2023-07-23 12:24:17 +0330

    fixed movile animation

--- project03/src/start/script.js
+++ project03/src/start/script.js
@@ -1,11 +1,27 @@
+let accardionOpen = false
 document.getElementById('arrow').addEventListener('click', () => {
-  console.log('test')
-  let children = Array.from(
+  const [, bot, , top] = Array.from(
     document.getElementById('arrow').parentElement.children
   )
-  children = [children[1], children[3]]
-  children.forEach(element => {
-    const ele = element
-    ele.classList.toggle('hidden')
-  })
+  const animateBot = () => {
+    top.removeEventListener('animationend', animateBot)
+    bot.classList.add('animate-slideBot')
+    bot.classList.remove('animate-slideBotOut')
+  }
+  const animateTopOut = () => {
+    bot.removeEventListener('animationend', animateTopOut)
+    top.classList.add('animate-slideTopOut')
+    top.classList.remove('animate-slideTop')
+  }
+  if (!accardionOpen) {
+    top.classList.add('animate-slideTop')
+    top.classList.remove('animate-slideTopOut')
+    top.addEventListener('animationend', animateBot)
+    accardionOpen = true
+  } else {
+    bot.classList.add('animate-slideBotOut')
+    bot.classList.remove('animate-slideBot')
+    bot.addEventListener('animationend', animateTopOut)
+    accardionOpen = false
+  }
 })
--- project03/tailwind.config.js
+++ project03/tailwind.config.js
@@ -10,6 +10,38 @@ module.exports = {
       colors: {
         lightpressed: '#D7D8DD',
       },
+      keyframes: {
+        slideIn: {
+          '0%': {
+            'max-height': 0,
+          },
+          '100%': {
+            'max-height': '400px',
+          },
+        },
+        slideOut: {
+          '0%': {
+            'max-height': '400px',
+          },
+          '100%': {
+            'max-height': 0,
+          },
+        },
+        slideOutmin: {
+          '0%': {
+            'max-height': '200px',
+          },
+          '100%': {
+            'max-height': 0,
+          },
+        },
+      },
+      animation: {
+        slideTop: 'slideIn 1s ease-in forwards',
+        slideBot: 'slideIn 1s ease-out forwards',
+        slideTopOut: 'slideOut 1s ease-out forwards',
+        slideBotOut: 'slideOutmin 0.5s ease-in forwards',
+      },
     },
   },
   plugins: [],
